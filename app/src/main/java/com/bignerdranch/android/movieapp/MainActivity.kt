72a7eb9a3e2646605acc9d97e2c2d45444dfa3fa
Joel Joshy <joeljoshy3243@gmail.com>
2023-11-09 17:25:29 -0800

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/movieapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.bignerdranch.android.movieapp
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.bignerdranch.android.movieapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+    }
+}
